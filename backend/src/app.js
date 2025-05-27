306d770a897005c80ff82b8c6138bcb55629c833
Sanjeet Mijar <your.ssnjitm6@gmail.com>
2025-05-27 18:51:59 +0545

    backend setupt part 1

---
+++ backend/src/app.js
@@ -0,0 +1,19 @@
+import cors from "cors";
+import express from "express";
+const app = express();
+
+//middlewares
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}))
+app.use(express.json({
+    limit: "16kb"
+}))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.static("public"))
+
+
+
+
+export { app };
