f8e651f371e3cefeaee5706e17fb7edad4cc1d5a
Thunder515 <gauravnepal1727@gmail.com>
2025-07-29 18:44:53 +0545

    GHJK

--- frontend/src/components/AboutPreview.jsx
+++ frontend/src/components/AboutPreview.jsx
@@ -2,7 +2,7 @@ const AboutPreview = () => {
   return (
     <section className="bg-white py-16 px-6 max-w-4xl mx-auto text-center">
       <h2 className="text-3xl font-bold text-sky-700 mb-4">About KhojSewa</h2>
-      <p className="text-gray-700">
+      <p className="text-purple-700">
         KhojSewa is a community-powered platform built to help you recover lost items and connect finders with rightful owners.
         With a fast and easy-to-use interface, anyone can post or search in seconds. Together, we can build a more helpful society.
       </p>
